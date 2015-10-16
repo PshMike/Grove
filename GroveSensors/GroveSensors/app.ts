a420003e8b9bb1989a7ec1880c887fbf035593b0
Mike Jenne <mike@whistler-consulting.com>
2015-10-16 14:48:00 -0500

    initial commit

---
+++ GroveSensors/GroveSensors/app.ts
@@ -0,0 +1,36 @@
+﻿enum GroveSensorCategory { ANALOG, DIGITAL, I2C }
+
+
+export class GroveSensor {
+    // Properties (public by default)
+
+    VCC: number;
+    category: GroveSensorCategory;
+
+    //constructor
+    constructor(cat: GroveSensorCategory, vcc?: number) {
+        this.VCC = vcc;
+        this.category = cat;
+
+        if (!this.VCC) {
+            this.VCC = 5;
+        }
+    }
+    
+    
+}
+
+export class GroveTemp extends GroveSensor {
+    // Properties (public by default)
+    version: number;
+
+    analogPin: number;
+
+    //constructor
+    constructor(pin: number, VCC: number) {
+        super(VCC,GroveSensorCategory.ANALOG);
+        this.analogPin = pin;
+    }
+
+
+}
