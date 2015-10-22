198ffa09bda32b408ba973897c33f5a4a9ec7a3b
Mike Jenne <mike@whistler-consulting.com>
2015-10-21 19:40:43 -0500

    first pass HT

--- NodejsConsoleApp1/NodejsConsoleApp1/grove-sensors.js
+++ NodejsConsoleApp1/NodejsConsoleApp1/grove-sensors.js
@@ -163,18 +163,64 @@ var MyGrove;
             this._pinAmbient = pinAmbient;
             this._pinThmc = pinThmc;
         }
-        Object.defineProperty(GroveHighTempSensor.prototype, "value", {
+        Object.defineProperty(GroveHighTempSensor.prototype, "rawAmbient", {
             get: function () {
-                return this._value;
+                return this._rawAmbient;
             },
             enumerable: true,
             configurable: true
         });
-        GroveHighTempSensor.prototype.update = function () {
+        Object.defineProperty(GroveHighTempSensor.prototype, "rawThmc", {
+            get: function () {
+                return this._rawThmc;
+            },
+            enumerable: true,
+            configurable: true
+        });
+        GroveHighTempSensor.prototype.UpdateAmbient = function () {
             var a;
-            a = this.getSample(this._pin);
-            this._value = a;
+            var resistance;
+            var temp;
+            a = this._rawAmbient * 50 / 33;
+            resistance = (1023 - a) * 10000 / a;
+            temp = 1 / (Math.log(resistance / 10000) / 3975 + 1 / 298.15) - 273.15;
+            this._tempAmbient = temp;
         };
+        GroveHighTempSensor.prototype.update = function () {
+            // read analog pins
+            this._rawAmbient = this.getSample(this._pinAmbient);
+            this._rawThmc = this.getSample(this._pinThmc);
+            this.UpdateAmbient();
+            // get voltage of thmc
+            this._voltage = ((this._rawThmc / 1023.0 * 5.0 * 1000) - GroveHighTempSensor.VOL_OFFSET) / GroveHighTempSensor.AMP_AV;
+            // convert voltage to K
+            var Var_VtoT_K = [[0, 2.5173462e1, -1.1662878, -1.0833638, -8.9773540 / 1e1, -3.7342377 / 1e1, -8.6632643 / 1e2, -1.0450598 / 1e2, -5.1920577 / 1e4],
+                [0, 2.508355e1, 7.860106 / 1e2, -2.503131 / 1e1, 8.315270 / 1e2, -1.228034 / 1e2, 9.804036 / 1e4, -4.413030 / 1e5, 1.057734 / 1e6, -1.052755 / 1e8],
+                [-1.318058e2, 4.830222e1, -1.646031, 5.464731 / 1e2, -9.650715 / 1e4, 8.802193 / 1e6, -3.110810 / 1e8]
+            ];
+            var i = 0;
+            var value = 0;
+            if (this._voltage >= -6.478 && this._voltage < 0) {
+                value = Var_VtoT_K[0][8];
+                for (i = 8; i > 0; i--)
+                    value = this._voltage * value + Var_VtoT_K[0][i - 1];
+            }
+            else if (this._voltage >= 0 && this._voltage < 20.644) {
+                value = Var_VtoT_K[1][9];
+                for (i = 9; i > 0; i--) {
+                    value = this._voltage * value + Var_VtoT_K[1][i - 1];
+                }
+            }
+            else if (this._voltage >= 20.644 && this._voltage <= 54.900) {
+                value = Var_VtoT_K[2][6];
+                for (i = 6; i > 0; i--)
+                    value = this._voltage * value + Var_VtoT_K[2][i - 1];
+            }
+            // update private members
+            this._tempThmc = value + this._tempAmbient;
+        };
+        GroveHighTempSensor.VOL_OFFSET = 350;
+        GroveHighTempSensor.AMP_AV = 54.16;
         return GroveHighTempSensor;
     })(GroveAnalogSensor);
     MyGrove.GroveHighTempSensor = GroveHighTempSensor;
--- NodejsConsoleApp1/NodejsConsoleApp1/grove-sensors.ts
+++ NodejsConsoleApp1/NodejsConsoleApp1/grove-sensors.ts
@@ -176,13 +176,18 @@
             this._value = a;
         }
     }
-    
+
     export class GroveHighTempSensor extends GroveAnalogSensor {
-        private _value: number;
         private _pinAmbient: Intel.IoT.Aio;
         private _pinThmc: Intel.IoT.Aio;
         private _tempThmc: number;
         private _tempAmbient: number;
+        private _rawAmbient: number;
+        private _rawThmc: number;
+        private _voltage: number;
+
+        private static VOL_OFFSET: number = 350;
+        private static AMP_AV: number = 54.16;
 
         constructor(pinAmbient: Intel.IoT.Aio, pinThmc: Intel.IoT.Aio, version: number, vcc: number) {
             super(version, vcc);
@@ -192,19 +197,74 @@
 
         }
 
-        public get value(): number {
-            return this._value;
+        public get rawAmbient(): number {
+            return this._rawAmbient;
+        }
+
+        public get rawThmc(): number {
+            return this._rawThmc;
+        }
+
+        private UpdateAmbient(): void {
+
+            var a: number;
+            var resistance: number;
+            var temp: number;
+
+            a = this._rawAmbient * 50 / 33;
+
+            resistance = (1023 - a) * 10000 / a;
+            temp = 1 / (Math.log(resistance / 10000) / 3975 + 1 / 298.15) - 273.15;
+
+            this._tempAmbient = temp;
         }
 
         public update(): void {
 
-            var a: number;
-            a = this.getSample(this._pin);
+            // read analog pins
 
-            this._value = a;
+            this._rawAmbient = this.getSample(this._pinAmbient);
+            this._rawThmc = this.getSample(this._pinThmc);
+
+            this.UpdateAmbient();
+            
+            // get voltage of thmc
+            this._voltage = ((this._rawThmc / 1023.0 * 5.0 * 1000) - GroveHighTempSensor.VOL_OFFSET) / GroveHighTempSensor.AMP_AV;
+
+            // convert voltage to K
+            var Var_VtoT_K = [[0, 2.5173462e1, -1.1662878, -1.0833638, -8.9773540 / 1e1, -3.7342377 / 1e1, -8.6632643 / 1e2, -1.0450598 / 1e2, -5.1920577 / 1e4],
+                [0, 2.508355e1, 7.860106 / 1e2, -2.503131 / 1e1, 8.315270 / 1e2, -1.228034 / 1e2, 9.804036 / 1e4, -4.413030 / 1e5, 1.057734 / 1e6, -1.052755 / 1e8],
+                [-1.318058e2, 4.830222e1, -1.646031, 5.464731 / 1e2, -9.650715 / 1e4, 8.802193 / 1e6, -3.110810 / 1e8]
+            ];
+
+            var i: number = 0;
+            var value: number = 0;
+
+            if (this._voltage >= -6.478 && this._voltage < 0) {
+                value = Var_VtoT_K[0][8];
+
+                for (i = 8; i > 0; i--)
+                    value = this._voltage * value + Var_VtoT_K[0][i - 1];
+            }
+            else if (this._voltage >= 0 && this._voltage < 20.644) {
+                value = Var_VtoT_K[1][9];
+
+                for (i = 9; i > 0; i--) {
+                    value = this._voltage * value + Var_VtoT_K[1][i - 1];
+                }
+            }
+            else if (this._voltage >= 20.644 && this._voltage <= 54.900) {
+                value = Var_VtoT_K[2][6];
+
+                for (i = 6; i > 0; i--)
+                    value = this._voltage * value + Var_VtoT_K[2][i - 1];
+            }
+
+            // update private members
+            this._tempThmc = value + this._tempAmbient;
+
         }
     }
-
 }
 
 
